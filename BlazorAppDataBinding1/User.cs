7711fe371ff0dcda19a2543a6b892d7420671c68
DELLL <DELLL@DESKTOP-84DSB3A>
2024-12-15 21:33:54 +0200

    Add project files.

---
+++ BlazorAppDataBinding1/User.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorAppDataBinding1
+{
+    public class User
+    {
+        private string username;
+        private string email;
+        private string password;
+
+        public string Username {  get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
